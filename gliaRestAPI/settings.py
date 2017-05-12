25772cff336d86df3a6ff34d5af359cc41374eb2
Samuel Resendez <saresend@usc.edu>
2017-05-12 08:24:51 -0700

    cors

--- gliaRestAPI/settings.py
+++ gliaRestAPI/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_lbwiso!vvcl+ng6*!1)^ogubz&w_2usv=)v7&18-gn2#e6$^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'glia-env.y5rqrbpijs.us-west-2.elasticbeanstalk.com',
+    'localhost',
 ]
 
 
